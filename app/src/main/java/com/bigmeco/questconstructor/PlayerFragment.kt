41202c21f3abcc439bec7d37f7f5c295d92299b9
bigmeco <bigmecoprog@gmail.com>
2018-10-24 13:50:13 +0300

    add anim fiter

--- app/src/main/java/com/bigmeco/questconstructor/PlayerFragment.kt
+++ app/src/main/java/com/bigmeco/questconstructor/PlayerFragment.kt
@@ -1,19 +1,16 @@
 package com.bigmeco.questconstructor
 
-import android.animation.AnimatorInflater
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
-import android.content.Context
-import android.net.Uri
+import android.content.res.Resources
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.DecelerateInterpolator
 import kotlinx.android.synthetic.main.fragment_player.*
 import android.view.animation.AnimationUtils
 import android.view.animation.Animation
+import android.graphics.drawable.ColorDrawable
+import android.widget.NumberPicker
 
 
 
@@ -29,12 +26,117 @@ class PlayerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val animScale = AnimationUtils.loadAnimation(activity, R.anim.genre_anim)
+
+        genrePicker.maxValue = 9
+        genrePicker.minValue = 0
+        setDividerColor(genrePicker, resources.getColor(R.color.pleyColor2))
+        setDividerColor(timerPicker, resources.getColor(R.color.pleyColor2))
+        setDividerColor(ratingPicker, resources.getColor(R.color.pleyColor2))
 
 
-        imageView4.setOnClickListener{
-            imageView4.startAnimation(animScale)
+        val animFlterOpen = AnimationUtils.loadAnimation(activity, R.anim.filter_anim)
+        animFlterOpen.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationEnd(animation: Animation) {
+                imageFilter.visibility = View.GONE
+                filterLayout.setBackgroundColor( resources.getColor(R.color.pleyColor))
+            }
 
+            override fun onAnimationRepeat(animation: Animation) {}
+
+            override fun onAnimationStart(animation: Animation) {
+            }
+        })
+        val animFlterClose = AnimationUtils.loadAnimation(activity, R.anim.filter_anim_beack)
+        animFlterClose.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationEnd(animation: Animation) {
+
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {}
+
+            override fun onAnimationStart(animation: Animation) {
+                imageFilter.visibility = View.VISIBLE
+                filterLayout.setBackgroundResource(R.drawable.full_filter_item)
+            }
+        })
+        val animScale = AnimationUtils.loadAnimation(activity, R.anim.menu_anim)
+        animScale.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationEnd(animation: Animation) {
+                imageMenu.visibility = View.GONE
+                menuLayout.setBackgroundColor( resources.getColor(R.color.pleyColor))
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {}
+
+            override fun onAnimationStart(animation: Animation) {
+            }
+        })
+        val animScaleBeack = AnimationUtils.loadAnimation(activity, R.anim.menu_anim_beack)
+        animScaleBeack.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationEnd(animation: Animation) {
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {}
+
+            override fun onAnimationStart(animation: Animation) {
+                imageMenu.visibility = View.VISIBLE
+                menuLayout.setBackgroundResource(R.drawable.full_menu_item)
+
+            }
+        })
+
+        imageBaeckF.setOnClickListener {
+            filterLayout.visibility = View.INVISIBLE
+            filterLayout.startAnimation(animFlterClose)
+
+
+        }
+        imageFilter.setOnClickListener {
+            filterLayout.visibility = View.VISIBLE
+            filterLayout.startAnimation(animFlterOpen)
+        }
+
+        imageOkF.setOnClickListener {
+            filterLayout.visibility = View.INVISIBLE
+            filterLayout.startAnimation(animFlterClose)
+        }
+
+        imageMenu.setOnClickListener {
+            menuLayout.visibility = View.VISIBLE
+            menuLayout.startAnimation(animScale)
+        }
+        imageBeack.setOnClickListener {
+            menuLayout.visibility = View.INVISIBLE
+            menuLayout.startAnimation(animScaleBeack)
+        }
+
+        imageOk.setOnClickListener {
+            menuLayout.visibility = View.INVISIBLE
+            menuLayout.startAnimation(animScaleBeack)
+        }
+
+
+    }
+
+    private fun setDividerColor(picker: NumberPicker, color: Int) {
+
+        val pickerFields = NumberPicker::class.java.declaredFields
+        for (pf in pickerFields) {
+            if (pf.name == "mSelectionDivider") {
+                pf.isAccessible = true
+                try {
+                    val colorDrawable = ColorDrawable(color)
+                    pf.set(picker, colorDrawable)
+                } catch (e: IllegalArgumentException) {
+                    e.printStackTrace()
+                } catch (e: Resources.NotFoundException) {
+                    e.printStackTrace()
+                } catch (e: IllegalAccessException) {
+                    e.printStackTrace()
+                }
+
+                break
+            }
         }
     }
 }
