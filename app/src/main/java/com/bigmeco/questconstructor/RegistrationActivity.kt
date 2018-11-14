9992cf26caff61b157bb0fd7b78b6788941336fe
bigmeco <bigmecoprog@gmail.com>
2018-11-14 13:16:48 +0300

    add auth google

---
+++ app/src/main/java/com/bigmeco/questconstructor/RegistrationActivity.kt
@@ -0,0 +1,85 @@
+package com.bigmeco.questconstructor
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_registration.*
+
+class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var mAuth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        mAuth = FirebaseAuth.getInstance()
+        singButton.setOnClickListener {
+
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                if (account != null) {
+                    firebaseAuthWithGoogle(account)
+                }
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+            }
+
+        }
+    }
+
+
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.id!!)
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        mAuth!!.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithCredential:success")
+                        val user = mAuth!!.currentUser
+                        //updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithCredential:failure", task.exception)
+                        //  Snackbar.make(findViewById(R.id.activity_login), "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
+                      //  updateUI(null)
+                    }
+
+                }
+    }
+    companion object {
+        private const val TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 9001
+    }
+}
---
+++ app/src/main/java/com/bigmeco/questconstructor/SplashActivity.kt
@@ -0,0 +1,57 @@
+ package com.bigmeco.questconstructor
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+ class SplashActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        scheduleSplashScreen()
+    }
+
+
+    private fun scheduleSplashScreen() {
+        val splashScreenDuration = getSplashScreenDuration()
+        Handler().postDelayed(
+                {
+                   // getRouteToAppropriatePage()
+                    startActivity(Intent(this, RegistrationActivity::class.java))
+                    finish()
+                },
+                splashScreenDuration
+        )
+    }
+
+    private fun getSplashScreenDuration() = 1000L
+
+    private fun getRouteToAppropriatePage() {
+//        launch(UI) {
+//            when (mSplashActivityVM.isUserLoggedIn()) {
+//                true -> {
+//                    startActivity(Intent(application, MenuActivity::class.java))
+//                    finish()
+//                }
+//                false -> {
+//                    startActivity(Intent(application, LoginActivity::class.java))
+//                    finish()
+//                }
+//            }
+//        }
+    }
+
+
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+}
