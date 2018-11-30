44200a8a9fb6bc70db352edecea1d3f9ca3da271
bigmeco <bigmecoprog@gmail.com>
2018-11-30 21:39:39 +0300

    add model project creation unit test

--- app/src/androidTest/java/com/bigmeco/questconstructor/AndroidCreatorTest.kt
+++ app/src/androidTest/java/com/bigmeco/questconstructor/AndroidCreatorTest.kt
@@ -3,9 +3,13 @@ package com.bigmeco.questconstructor
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import com.bigmeco.questconstructor.data.ObjectProject
+import com.bigmeco.questconstructor.data.ObjectScreen
 import com.bigmeco.questconstructor.models.CreatorModel
+import com.bigmeco.questconstructor.models.ProjectCreationModel
 import io.realm.Realm
-import org.junit.Assert.assertEquals
+import io.realm.RealmList
+import org.junit.Assert
+
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,15 +17,27 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class AndroidCreatorTest {
 
+
+
+
     @Test
     fun getListTest() {
-        val test = ArrayList<ObjectProject>()
         val appContext = InstrumentationRegistry.getTargetContext()
         Realm.init(appContext)
-        val realm: Realm = Realm.getDefaultInstance()
+         val realm: Realm = Realm.getDefaultInstance()
+
+        val test = ArrayList<ObjectProject>()
         val creatorModel = CreatorModel()
         test.addAll(realm.where(ObjectProject::class.java).findAll())
         test.add(ObjectProject())
-        assertEquals(test[0], creatorModel.getProjects()[0])
+        Assert.assertEquals(test[0], creatorModel.getProjects()[0])
+    }
+    @Test
+    fun setProjectTest() {
+        val appContext = InstrumentationRegistry.getTargetContext()
+        Realm.init(appContext)
+        val projectCreationModel= ProjectCreationModel()
+         val objectProject = ObjectProject()
+        Assert.assertNotNull(projectCreationModel.getStandardProject(objectProject))
     }
 }
